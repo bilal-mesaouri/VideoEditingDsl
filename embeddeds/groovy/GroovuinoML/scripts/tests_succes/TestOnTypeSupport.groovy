ad468a3d7d7acf4a4ca0962b189cf2f3e4555256
sikakaleb <dassou-ablam-kaleb.sika@etu.univ-cotedazur.fr>
2025-01-15 14:36:41 +0100

    use wav instead of mp3

--- embeddeds/groovy/GroovuinoML/scripts/tests_succes/TestOnTypeSupport.groovy
+++ embeddeds/groovy/GroovuinoML/scripts/tests_succes/TestOnTypeSupport.groovy
@@ -1,9 +1,9 @@
 // Vidéo avec durée numérique
 video "videoClip1" path "video1.mp4" duration 10
 // Audio avec durée numérique
-audio "introAudio" path "intro.mp3" duration 5
+audio "introAudio" path "intro.wav" duration 5
 // Audio avec durée en chaîne
-audio "explanation1" path "explanation1.mp3" duration "8"
+audio "explanation1" path "explanation1.wav" duration "8"
 
 // Ajustement du volume avec chaîne
 adjustVolume "adjustedAudio1" audio introAudio volumeFactor "0.8"
